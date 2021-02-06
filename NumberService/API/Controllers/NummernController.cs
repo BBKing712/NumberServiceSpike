780d8d5eebf9ee359db1b5acb8f70d7e137a3d02
Bernhard <b.korn@boservice.de>
2021-02-06 20:51:49 +0100

    NummernController for all.

---
+++ NumberService/API/Controllers/NummernController.cs
@@ -0,0 +1,42 @@
+﻿using API.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+using API.Models;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class NummernController
+    {
+        private readonly NumberserviceContext _context;
+
+        public NummernController(NumberserviceContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Nummern/HoleDatentypen
+        [HttpGet("HoleDatentypen")]
+        public async Task<ActionResult<IEnumerable<Datentyp>>> HoleDatentypen()
+        {
+            return await _context.Datentyps.ToListAsync();
+        }
+        // GET: api/Nummern/HoleNummerDefinitionen
+        [HttpGet("HoleNummerDefinitionen")]
+        public async Task<ActionResult<IEnumerable<NummerDefinition>>> HoleNummerDefinitionen()
+        {
+            var tmp = _context.NummerDefinitions.Include(e => e.NummerDefinitionQuelles);
+
+            return await _context.NummerDefinitions.ToListAsync();
+        }
+
+
+    }
+}
