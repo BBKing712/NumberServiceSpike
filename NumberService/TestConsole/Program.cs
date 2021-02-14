146fcc00e6dcb69d40dd3679c723aaded4eb4560
Bernhard <b.korn@boservice.de>
2021-02-14 14:52:35 +0100

    TestConsole added

---
+++ NumberService/TestConsole/Program.cs
@@ -0,0 +1,27 @@
+﻿using Common.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace TestConsole
+{
+    //see
+    //https://www.yogihosting.com/aspnet-core-consume-api/#readid
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            List<NummerDefinition> nummerDefinitionList = new List<NummerDefinition>();
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync("http://localhost:51868/api/Nummern/HoleNummerDefinitionen"))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    nummerDefinitionList = JsonConvert.DeserializeObject<List<NummerDefinition>>(apiResponse);
+                }
+            }
+        }
+    }
+}
